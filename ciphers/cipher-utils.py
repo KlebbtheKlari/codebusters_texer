c9fe37ea6a4c4eae61a34c10beebb29e49544404
Caleb Chiang <calebrong@gmail.com>
2024-07-20 16:41:27 -0700

    cipher-utils: num_to_letter, letter_to_num, string_to_nums

--- ciphers/cipher-utils.py
+++ ciphers/cipher-utils.py
@@ -13,6 +13,7 @@ import re
 def strip_repeats(s):
     return "".join(dict.fromkeys(s))
 
+
 # removes all non-alphabetic characters from s
 # capitalizes all letters of s
 # input: a string
@@ -23,12 +24,34 @@ def answerize(s):
     ret = regex.sub('',s)
     return ret.upper()\
 
+
 # returns the modular inverse of a modulo m
 def mod_inverse(a,m):
     return pow(a,-1,m)
+
 
 # returns the corresponding letter for x
 # using A=0, B=1, ... Z=25
 # if x > 26, then letter is returned according to x mod 26
 def A0Z25(x):
     return chr((x%26) + 65)
+
+def num_to_letter(x):
+    return A0Z25(x)
+
+# returns the corresponding number for x
+# using A=0, B=1, ... Z=25
+# input: x is a single upper-case letter
+def letter_to_num(x):
+    return (ord(x)-65)
+
+
+# returns the corresponding numbers for a string s
+# input: string
+# output: list of numbers corresponding letter-by-letter of s
+def string_to_nums(s):
+    letters = answerize(s)
+    ret = []
+    for i in list(letters):
+        ret.append(letter_to_num(i))
+    return ret
