9c2dc194b830d0c1476eefc41f7ed5d73f86ed2e
Caleb Chiang <calebrong@gmail.com>
2024-07-20 17:01:03 -0700

    caesar, affine

--- ciphers/affine.py
+++ ciphers/affine.py
@@ -1,18 +1,18 @@
 ######
-# Atbash cipher!
+# Affine cipher!
 ######
 
 import re
 from cipher_utils import *
 
-def atbash_encode(s):
+def affine_encode(s,A,B):
     ret = ''
     l = list(s)
     
     for i in l:
         if (re.search("[a-zA-Z]",i)):
             i = i.upper()
-            ret += num_to_letter(25-letter_to_num(i))
+            ret += num_to_letter(A*letter_to_num(i)+B)
         else:
             ret += i
     return ret
--- ciphers/caesar.py
+++ ciphers/caesar.py
@@ -1,18 +1,18 @@
 ######
-# Atbash cipher!
+# Caesar cipher!
 ######
 
 import re
 from cipher_utils import *
 
-def atbash_encode(s):
+def caesar_encode(s,shift):
     ret = ''
     l = list(s)
     
     for i in l:
         if (re.search("[a-zA-Z]",i)):
             i = i.upper()
-            ret += num_to_letter(25-letter_to_num(i))
+            ret += num_to_letter(letter_to_num(i)+shift)
         else:
             ret += i
     return ret
