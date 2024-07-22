e96d31dcb287d1f76417d4ee8375e5224eebe57a
Caleb Chiang <calebrong@gmail.com>
2024-07-21 17:04:17 -0700

    monoalphabetic

--- src/monoalphabetic.py
+++ src/monoalphabetic.py
@@ -5,8 +5,24 @@
 ######
 
 import random
+import re
 
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+class Monoalphabetic:
+    def __init__(self, plaintext,value,cipher,type,alphabet=None):
+        self.pt = plaintext
+        self.val = value
+        self.cipher = cipher
+        self.type = type
+        
+        if (alphabet == None):
+            self.alphabet = ""
+        else:
+            self.alphabet = alphabet
+        # Generate pt-ct mapping
+        # Encode (& parse if pat), store ct
+
 
 # generates a random alphabet
 # ensures that no letter is in the same position
@@ -25,15 +41,24 @@ def gen_random_alphabet():
     
     return ''.join(alpha)
 
-
+# transforms s to ciphertext
 def aristo_encoder(s, pt_alphabet, ct_alphabet):
     mapping = {}
     pt = list(pt_alphabet)
     ct = list(ct_alphabet)
-    for i in pt:
-        mapping[i] = ct[i]
+    ret = ''
+    for i in range(len(pt)):
+        mapping[pt[i]] = ct[i]
+    
+    l = list(s)
+    for i in l:
+        if (re.search("[a-zA-Z]",i)):
+            i = i.upper()
+            ret += mapping[i]
+        else:
+            ret += i
+    return ret
 
 def random_aristo(s):
     ct = gen_random_alphabet()
     return aristo_encoder(s,alphabet,ct)
-
