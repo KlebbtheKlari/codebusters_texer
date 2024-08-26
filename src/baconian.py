fb59d0a6594c5e4e48320e85d8ef444fb10ff778
Caleb Chiang <calebrong@gmail.com>
2024-08-26 10:31:37 -0500

    letter bacon + words.txt

--- src/baconian.py
+++ src/baconian.py
@@ -6,12 +6,11 @@ from cipher_utils import *
 
 class Baconian:
     
-    def __init__(self, type,val,plaintext,key,bonus,amt=None,crib=None):
+    def __init__(self, type,val,plaintext,bonus,amt=None,crib=None):
         self.type = answerize(type)
         self.val = int(val)
         self.pt = answerize(plaintext)
-        self.key = answerize(key)
-        self.bin = self.baconian_encode(self.pt, self.key)
+        self.bin = self.baconian_encode(self.pt)
         self.bonus = bonus # bool
         if (crib == None):
             pass
@@ -25,20 +24,38 @@ class Baconian:
             self.amt = int(amt)
             
         if (self.type == "LETTER"):
-            self.ct = self.bin
+            self.ct = self.baconian_letters(self.bin,self.amt)
         elif (self.type == "SEQUENCE"):
             self.ct = 'seq'
         else:
             self.ct = 'word'
             
         
-    def baconian_encode(self,pt,key):
+    def baconian_encode(self,pt):
         ret = ''
         
-        for i in pt:            
+        # cipher-utils.baconify()
+        # converts to binary, NOT A/B
+        for i in pt:
             ret += baconify(i)
         
         return ret
     
-f = Baconian('letter',1,alphabet,'sleight',False,2,'crib')
+    def baconian_letters(self,ct,amt):
+        a = gen_random_alphabet()
+        a_lets = []
+        b_lets = []
+        for i in range(amt):
+            a_lets.append(a[i])
+            b_lets.append(a[amt+i])
+        
+        ret = ''
+        for i in ct:
+            if i=='0':
+                ret += a_lets[random.randint(0,amt-1)]
+            else:
+                ret += b_lets[random.randint(0,amt-1)]
+        return ret
+    
+f = Baconian('letter',1,"XLB is tasty",False,2,'crib')
 print(blockify(f.ct,5))
